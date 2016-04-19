36cd922a30573b556822ab7849283f65b58e9a74
Bilbobx182 <c14474048@mydit.ie>
2016-04-19 13:27:46 +0100

    VERSION 1.0.3

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ import sys
 import spotipy.util as util
 
 #"Declaring" variables for the first time.
-user_agent = ("")
-username =""
+user_agent = ("bilbobx182crawler")
+username ="eternal_atom"
 tid = ""
-pid = ""
+pid = "03VDb9C66xFlisFCMqxgwz"
 scope = 'playlist-modify-public'
 
-Cid = ""
-Cs = ""
+Cid = "1b2904aec1c64e45941b35bfe7a66792"
+Cs = "a7f0819ab0fa4cee99d7d8f3a34d6193"
 SRI = "http://localhost:8888/callback"
 onetrack = 0
 track_ids = []
-sub = "emo"
+sub = "music"
 
 r = praw.Reddit(user_agent = user_agent)
 
@@ -35,7 +35,7 @@ else:
 subreddit = r.get_subreddit(sub)
 counter=0
 # the main loop of the program, it tells it as long as there's a submission in the top X amount loop around.
-for submission in subreddit.get_top_from_week(limit=35):
+for submission in subreddit.get_top_from_week(limit=1000):
     num = submission.title.find(' - ')
     if num != -1:
         with open("songlist.txt", "a") as f:
@@ -45,7 +45,7 @@ for submission in subreddit.get_top_from_week(limit=35):
                 submission.title.replace("-","")
                 search_str = submission.title
 
-            # Checking to see if the song has any extra stuff after artist - song generally extra is tagged with () so we remove them.
+                # Checking to see if the song has any extra stuff after artist - song generally extra is tagged with () so we remove them.
             if any("(" or ")" in lis for lis in search_str):
                 start = search_str.find('(')
                 end = search_str.find(')')
@@ -76,11 +76,17 @@ for submission in subreddit.get_top_from_week(limit=35):
                         onetrack = onetrack + 1
                         f.write(t['id'])
                         f.write("\n")
-                        token = util.prompt_for_user_token(username, scope, Cid, Cs, SRI)
-                        if token:
-                            sp = spotipy.Spotify(auth=token)
-                            sp.trace = False
+
+#so the program can handle high amounts of tracks
+        if (len(track_ids) > 80):
+            token = util.prompt_for_user_token(username, scope, Cid, Cs, SRI)
+            if token:
+                sp = spotipy.Spotify(auth=token)
+                sp.trace = False
+                results = sp.user_playlist_add_tracks(username, pid, track_ids)
+                print(len(track_ids))
+                track_ids.clear()
         onetrack = 0
-print(counter)
+print(len(track_ids))
 #Adding the track to spotify
-results = sp.user_playlist_add_tracks(username, pid, track_ids)
+
