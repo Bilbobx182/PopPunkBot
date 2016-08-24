b67337b4a3bc6a77f9f116f159c1da6edddb0ced
Bilbobx182 <c14474048@mydit.ie>
2016-08-24 19:39:36 +0100

    V.1.0.7

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ onetrack = 0
 track_ids = []
 sub = "poppunkers"
 
+
 r = praw.Reddit(user_agent = user_agent)
 
 # Checking to see if a text file for song list(They're the spotify URI codes) is there
@@ -33,143 +34,87 @@ else:
         songlist = filter(None, songlist)
 
 
+def titleparse(songdata,num):
+
+    info=songdata
+    global onetrack
+    global track_ids
+
+    if(num==1):
+        response = requests.get(songdata)
+        soup = BeautifulSoup(response.content, "html.parser")
+        title = soup.title.string
+        title = title.split('-')
+        titlelen = len(title)
+        fixedtitle = ""
+        i = 0
+        while (i < (titlelen - 1)):
+            fixedtitle = fixedtitle + title[i]
+            i += 1
+        info = fixedtitle
+
+    if (" - " in info):
+        info.replace("-", "")
+
+    if any("(" or ")" in lis for lis in info):
+        start = info.find('(')
+        end = info.find(')')
+        if start != -1 and end != -1:
+            result = info[start:end + 1]
+            info=info.replace(result, "")
+
+    # however some users submit it in a wrong format with [] instead so we remove those too
+    if any("[" or "]" in lis for lis in info):
+        start = info.find('[')
+        end = info.find(']')
+        if start != -1 and end != -1:
+            result = info[start:end + 1]
+            info = info.replace(result, "")
+
+    sp = spotipy.Spotify()
+    # Creating the spotify query to see if a song is in it's database.
+    with open("songlist.txt", "a") as f:
+        result = sp.search(info)
+        for i, t in enumerate(result['tracks']['items']):
+            # gets the first track ID and only that ID so it wont repeat through album or singles
+            if (onetrack <= 0):
+                temp = t['id']
+                if (temp in songlist):
+                    # For Duplicates
+                    print("ITS HERE ALREADY")
+                    break
+                else:
+                    track_ids.append(t['id'])
+                    onetrack = onetrack + 1
+                    f.write(t['id'])
+                    f.write("\n")
+                    
+    print("DONE: " + info)
+
 # tells it to go to /r/poppunkers
 subreddit = r.get_subreddit(sub)
 counter=0
+
 # the main loop of the program, it tells it as long as there's a submission in the top X amount loop around.
 for submission in subreddit.get_top_from_week(limit=300):
     num = submission.title.find(' - ')
     if num != -1:
-        with open("songlist.txt", "a") as f:
-            # The format for songs is "Artist - Song ()" This way we can assume MOST titles that have a " - " are a song.
-            if(" - " in submission.title):
-                counter=counter+1
-                submission.title.replace("-","")
-                search_str = submission.title
+        titleparse(submission.title,0)
 
-                # Checking to see if the song has any extra stuff after artist - song generally extra is tagged with () so we remove them.
-            if any("(" or ")" in lis for lis in search_str):
-                start = search_str.find('(')
-                end = search_str.find(')')
-                if start != -1 and end != -1:
-                    result = search_str[start:end + 1]
-                    search_str = search_str.replace(result,"")
-            else:
-                search_str = submission.title
-
-            # however some users submit it in a wrong format with [] instead so we remove those too
-            if any("[" or "]" in lis for lis in search_str):
-                start = search_str.find('[')
-                end = search_str.find(']')
-                if start != -1 and end != -1:
-                    result = search_str[start:end + 1]
-                    search_str = search_str.replace(result, "")
-            else:
-                search_str=submission.title
-            print(search_str)
-
-            # Creating the spotify query to see if a song is in it's database.
-            sp = spotipy.Spotify()
-            result = sp.search(search_str)
-            for i, t in enumerate(result['tracks']['items']):
-                # gets the first track ID and only that ID so it wont repeat through album or singles
-                if (onetrack <= 0):
-                    temp = t['id']
-                    if(temp in songlist):
-                        #For Duplicates
-                        print("ITS HERE ALREADY")
-                        break
-                    else:
-                        track_ids.append(t['id'])
-                        onetrack = onetrack + 1
-                        f.write(t['id'])
-                        f.write("\n")
-
-                            #so the program can handle high amounts of tracks
-        if (len(track_ids) > 80):
-            token = util.prompt_for_user_token(username, scope, Cid, Cs, SRI)
-            if token:
-                sp = spotipy.Spotify(auth=token)
-                sp.trace = False
-                results = sp.user_playlist_add_tracks(username, pid, track_ids)
-                print(len(track_ids))
-                track_ids.clear()
-        onetrack = 0
     elif('youtu' in submission.url):
-        response = requests.get(submission.url)
-        soup = BeautifulSoup(response.content, "html.parser")
-        print(soup.title.string)
-        title = soup.title.string
-        title = title.split('-')
-        print(title)
-        titlelen = 0
-        titlelen = len(title)
-        fixedtitle=""
-        i = 0
-        while (i < (titlelen - 1)):
-            print(title[i])
-            fixedtitle=fixedtitle+title[i]
-            i += 1
+       titleparse(submission.url,1)
 
-        with open("songlist.txt", "a") as f:
-            # The format for songs is "Artist - Song ()" This way we can assume MOST titles that have a " - " are a song.
-            if (" - " in  fixedtitle):
-                counter = counter + 1
-                fixedtitle.replace("-", "")
-            search_str = fixedtitle
+    if (len(track_ids) > 80):
+        token = util.prompt_for_user_token(username, scope, Cid, Cs, SRI)
+        if token:
+            sp = spotipy.Spotify(auth=token)
+            sp.trace = False
+            results = sp.user_playlist_add_tracks(username, pid, track_ids)
+            print(len(track_ids))
+            track_ids.clear()
+    onetrack = 0
 
-                # Checking to see if the song has any extra stuff after artist - song generally extra is tagged with () so we remove them.
-            if any("(" or ")" in lis for lis in search_str):
-                start = search_str.find('(')
-                end = search_str.find(')')
-                if start != -1 and end != -1:
-                    result = search_str[start:end + 1]
-                    search_str = search_str.replace(result, "")
-            else:
-                search_str = submission.title
-
-            # however some users submit it in a wrong format with [] instead so we remove those too
-            if any("[" or "]" in lis for lis in search_str):
-                start = search_str.find('[')
-                end = search_str.find(']')
-                if start != -1 and end != -1:
-                    result = search_str[start:end + 1]
-                    search_str = search_str.replace(result, "")
-            else:
-                search_str = submission.title
-            print(search_str)
-
-            # Creating the spotify query to see if a song is in it's database.
-            sp = spotipy.Spotify()
-            result = sp.search(search_str)
-            for i, t in enumerate(result['tracks']['items']):
-                # gets the first track ID and only that ID so it wont repeat through album or singles
-                if (onetrack <= 0):
-                    temp = t['id']
-                    if (temp in songlist):
-                        # For Duplicates
-                        print("ITS HERE ALREADY")
-                        break
-                    else:
-                        track_ids.append(t['id'])
-                        onetrack = onetrack + 1
-                        f.write(t['id'])
-                        f.write("\n")
-
-                        # so the program can handle high amounts of tracks
-        if (len(track_ids) > 80):
-            token = util.prompt_for_user_token(username, scope, Cid, Cs, SRI)
-            if token:
-                sp = spotipy.Spotify(auth=token)
-                sp.trace = False
-                results = sp.user_playlist_add_tracks(username, pid, track_ids)
-                print(len(track_ids))
-                track_ids.clear()
-        onetrack = 0
-
-
-
-#dealing with tracks that wouldn't be submitted as the code above only deals with 80 per time but this is for smaller requests
+        #dealing with tracks that wouldn't be submitted as the code above only deals with 80 per time but this is for smaller requests
 if(len(track_ids) <= 80 and len(track_ids)>0):
     token = util.prompt_for_user_token(username, scope, Cid, Cs, SRI)
     if token:
