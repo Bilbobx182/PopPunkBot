19d8d4e032912ba07658f3375a25cb9931d40d1a
Ciaran Ó Nuallain <Ciaran  Nuallain>
2017-06-27 20:50:35 +0100

    Update so now it conforms with new PRAW and SpotifyAPI

--- main.py
+++ main.py
@@ -5,23 +5,33 @@ import spotipy
 import spotipy.util as util
 from bs4 import BeautifulSoup
 import requests
+import sys
 
-#Reddit variables
-user_agent = ("bilbobx182crawler")
-sub = "poppunkers"
-r = praw.Reddit(user_agent = user_agent)
+# "Declaring" variables for the first time.
 
-# Spotify variables
-pid = "6GXK27XsRSngHWROTtOsst" #playlist id
-username ="eternal_atom" #Spotify username
-scope = 'playlist-modify-private' #scope of playlist
-Cid = "1b2904aec1c64e45941b35bfe7a66792" # Spotify ID
-Cs = "a7f0819ab0fa4cee99d7d8f3a34d6193" # Spotify secret ID
-SRI = "http://localhost:8888/callback" # Host
+# Praw Variables
 
+username = "eternal_atom"
+tid = ""
+prawClientID = "reddit client ID here"
+
+# Spotify Variables
+pid = "playlist here"
+scope = 'playlist-modify-private'
+
+Cid = "clientID here"
+Cs = "client Secret here"
+SRI = "http://localhost:8888/callback"
 onetrack = 0
 track_ids = []
+sub = "metal"
 
+r = praw.Reddit(client_id="", client_secret='', password='',
+                user_agent='Reddit to Spotify converter', username="")
+
+token = util.prompt_for_user_token(username, scope, Cid, Cs, SRI)
+
+# Checking to see if a text file for song list(They're the spotify URI codes) is there
 if not os.path.isfile("songlist.txt"):
     songlist = []
 else:
@@ -31,14 +41,12 @@ else:
         songlist = filter(None, songlist)
 
 
-def titleparse(songdata,num):
-
-    info=songdata
+def titleparse(songdata, num):
+    info = songdata
     global onetrack
     global track_ids
-    
-    # Only used if it's a link and the title is wrong AND it's a youtube link so it gets the YT video title.
-    if num==1:
+
+    if (num == 1):
         response = requests.get(songdata)
         soup = BeautifulSoup(response.content, "html.parser")
         title = soup.title.string
@@ -51,7 +59,7 @@ def titleparse(songdata,num):
             i += 1
         info = fixedtitle
 
-    if " - " in info:
+    if (" - " in info):
         info.replace("-", "")
 
     if any("(" or ")" in lis for lis in info):
@@ -59,7 +67,7 @@ def titleparse(songdata,num):
         end = info.find(')')
         if start != -1 and end != -1:
             result = info[start:end + 1]
-            info=info.replace(result, "")
+            info = info.replace(result, "")
 
     # however some users submit it in a wrong format with [] instead so we remove those too
     if any("[" or "]" in lis for lis in info):
@@ -72,38 +80,40 @@ def titleparse(songdata,num):
     sp = spotipy.Spotify()
     # Creating the spotify query to see if a song is in it's database.
     with open("songlist.txt", "a") as f:
-        result = sp.search(info)
-        for i, t in enumerate(result['tracks']['items']):
-            # gets the first track ID and only that ID so it wont repeat through album or singles
-            if onetrack <= 0:
-                temp = t['id']
-                if temp in songlist:
-                    # For Duplicates
-                    print("ITS HERE ALREADY")
-                    break
-                else:
-                    track_ids.append(t['id'])
-                    onetrack = onetrack + 1
-                    f.write(t['id'])
-                    f.write("\n")
+        if token:
+            sp = spotipy.Spotify(auth=token)
+            result = sp.search(info)
+            for i, t in enumerate(result['tracks']['items']):
+                # gets the first track ID and only that ID so it wont repeat through album or singles
+                if (onetrack <= 0):
+                    temp = t['id']
+                    if (temp in songlist):
+                        # For Duplicates
+                        print("ITS HERE ALREADY")
+                        break
+                    else:
+                        track_ids.append(t['id'])
+                        onetrack = onetrack + 1
+                        f.write(t['id'])
+                        f.write("\n")
 
-    print("DONE: " + info)
+        print("DONE: " + info)
+
 
 # tells it to go to /r/poppunkers
-subreddit = r.get_subreddit(sub)
-counter=0
+subreddit = r.subreddit(sub)
+counter = 0
 
 # the main loop of the program, it tells it as long as there's a submission in the top X amount loop around.
-for submission in subreddit.get_top_from_week(limit=300):
+for submission in subreddit.top('week'):
     num = submission.title.find(' - ')
     if num != -1:
         titleparse(submission.title, 0)
 
-    elif 'youtu' in submission.url:
+    elif ('youtu' in submission.url):
         titleparse(submission.url, 1)
 
-    if len(track_ids) > 80:
-        token = util.prompt_for_user_token(username, scope, Cid, Cs, SRI)
+    if (len(track_ids) > 80):
         if token:
             sp = spotipy.Spotify(auth=token)
             sp.trace = False
@@ -112,7 +122,8 @@ for submission in subreddit.get_top_from_week(limit=300):
             track_ids.clear()
     onetrack = 0
 
-if 80 >= len(track_ids) > 0:
+    # dealing with tracks that wouldn't be submitted as the code above only deals with 80 per time but this is for smaller requests
+if (len(track_ids) <= 80 and len(track_ids) > 0):
     token = util.prompt_for_user_token(username, scope, Cid, Cs, SRI)
     if token:
         sp = spotipy.Spotify(auth=token)
